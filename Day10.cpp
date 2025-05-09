74b6b1ecfb93100150e181719d0d64be0ca46e0f
Akarsh Mishra <akarsh.23bcy10199@vitbhopal.ac.in>
2025-05-09 13:04:11 +0530

    Add files via upload

---
+++ Day10.cpp
@@ -0,0 +1,29 @@
+Bulb Switcher
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
+
+Delete Middle NOde
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (!head || !head->next)
+            return nullptr;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = slow->next;
+        delete slow; 
+
+        return head;
+    }
+};
